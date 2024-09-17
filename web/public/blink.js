7c53efebb5e1e1be7b2bc6bfc46decd0a3333dc6
Matt Anderson <matta9001@gmail.com>
2024-09-17 13:54:21 -0500

    blink after page switch

---
+++ web/public/blink.js
@@ -0,0 +1,12 @@
+try {
+	const blink1 = document.getElementById('blink1');
+	const blink2 = document.getElementById('blink2');
+	const blink3 = document.getElementById('blink3');
+
+	setInterval(() => {
+	  blink1.style.visibility = blink1.style.visibility === 'hidden' ? 'visible' : 'hidden';
+	  blink2.style.visibility = blink2.style.visibility === 'hidden' ? 'visible' : 'hidden';
+	  blink3.style.visibility = blink3.style.visibility === 'hidden' ? 'visible' : 'hidden';
+	}, 1000);
+} catch {}
+
