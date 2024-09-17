de6b9aab2d82f9fccb59391316abe26104076d9d
Matt Anderson <matta9001@gmail.com>
2024-09-17 15:52:33 -0500

    blink with hole square, just like my real shell

--- web/public/blink.js
+++ web/public/blink.js
@@ -2,11 +2,15 @@ try {
 	const blink1 = document.getElementById('blink1');
 	const blink2 = document.getElementById('blink2');
 	const blink3 = document.getElementById('blink3');
+	const blink4 = document.getElementById('blink4');
+	const blink5 = document.getElementById('blink5');
 
 	setInterval(() => {
 	  blink1.style.visibility = blink1.style.visibility === 'hidden' ? 'visible' : 'hidden';
 	  blink2.style.visibility = blink2.style.visibility === 'hidden' ? 'visible' : 'hidden';
 	  blink3.style.visibility = blink3.style.visibility === 'hidden' ? 'visible' : 'hidden';
+	  blink4.style.visibility = blink4.style.visibility === 'hidden' ? 'visible' : 'hidden';
+	  blink5.style.visibility = blink5.style.visibility === 'hidden' ? 'visible' : 'hidden';
 	}, 2000);
 } catch {}
 
