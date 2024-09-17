8b7fbc1bce6f99f387a5511363f2c0480410881f
Matt Anderson <matta9001@gmail.com>
2024-09-17 15:55:50 -0500

    Spacing

--- web/public/blink.js
+++ web/public/blink.js
@@ -1,16 +1,11 @@
 try {
-	const blink1 = document.getElementById('blink1');
-	const blink2 = document.getElementById('blink2');
-	const blink3 = document.getElementById('blink3');
-	const blink4 = document.getElementById('blink4');
-	const blink5 = document.getElementById('blink5');
+const blink1 = document.getElementById('blink1');
+const blink2 = document.getElementById('blink2');
+const blink3 = document.getElementById('blink3');
 
-	setInterval(() => {
-	  blink1.style.visibility = blink1.style.visibility === 'hidden' ? 'visible' : 'hidden';
-	  blink2.style.visibility = blink2.style.visibility === 'hidden' ? 'visible' : 'hidden';
-	  blink3.style.visibility = blink3.style.visibility === 'hidden' ? 'visible' : 'hidden';
-	  blink4.style.visibility = blink4.style.visibility === 'hidden' ? 'visible' : 'hidden';
-	  blink5.style.visibility = blink5.style.visibility === 'hidden' ? 'visible' : 'hidden';
-	}, 2000);
+setInterval(() => {
+  blink1.style.visibility = blink1.style.visibility === 'hidden' ? 'visible' : 'hidden';
+  blink2.style.visibility = blink2.style.visibility === 'hidden' ? 'visible' : 'hidden';
+  blink3.style.visibility = blink3.style.visibility === 'hidden' ? 'visible' : 'hidden';
+}, 1000);
 } catch {}
-
