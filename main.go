0885fc0ed956a590b9989abc9ba87210e64a7a6d
Matt Anderson <matta9001@gmail.com>
2024-09-17 13:00:40 -0500

    change to inconsolata and cool term style

--- main.go
+++ main.go
@@ -8,17 +8,18 @@ import (
 )
 
 func main() {
-	port := flag.String("port", "8080", "Port to run HTTP server on.")
+	port := flag.String("port", "8090", "Port to run HTTP server on.")
 	flag.Parse()
 
+	tmpl := template.Must(template.New("").ParseGlob("web/templates/*"))
+
 	mux := http.NewServeMux()
-	mux.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources"))))
+	mux.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("web/public"))))
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.New("").ParseFiles("resources/index.html")
-		if err != nil {
-			panic(err)
-		}
 		tmpl.ExecuteTemplate(w, "index.html", nil)
+	})
+	mux.HandleFunc("/studies", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.ExecuteTemplate(w, "studies.html", nil)
 	})
 
 	fmt.Println("Listening on :" + *port)
